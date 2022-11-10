bcef499f2707c3bf3f948f516b36fb346288a8d5
Tim Joyce <tim@evendrop.com>
2022-11-10 08:52:03 -0500

    -

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -143,63 +143,58 @@
     </div>
 
     <div
-    id="new-team-modal"
+    id="authentication-modal"
     tabindex="-1"
     :class="showCreateTeam ? '' : 'hidden'"
-    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center bg-gray-500 bg-opacity-60"
->
-        <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-            <div class="relative bg-white rounded-lg shadow  bg-gray-700">
-                <button
-                type="button"
-                class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-100 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center  hover:bg-gray-800  hover:text-white"
-                @click="showCreateTeam = false"
-                >
-                    <svg
-                        aria-hidden="true"
-                        class="w-5 h-5"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                        fill-rule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                        ></path>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-                <div class="py-6 px-6 lg:px-8">
-                    <h3 class="mb-4 text-xl font-medium text-gray-900  text-white">
-                        Create new team
-                    </h3>
-                    <form class="space-y-6" @submit.prevent="createNewTeam()">
-                        <div>
-                            <label
-                                for="email"
-                                class="block mb-2 text-sm font-medium text-gray-200  text-gray-300"
-                                >Team name</label
-                            >
-                            <input
-                                type="text"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  bg-gray-600  border-gray-500  placeholder-gray-400  text-white"
-                                placeholder="my new team"
-                                v-model="newTeamName"
-                            />
-                        </div>
+    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center bg-black bg-opacity-60"
+  >
+		<div class="relative p-4 w-full max-w-md h-full md:h-auto">
+			<div class="relative rounded-lg shadow  bg-gray-800">
+				<button
+				type="button"
+				class="absolute top-3 right-2.5 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+				@click="showCreateTeam = false"
+				>
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+						fill-rule="evenodd"
+						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+						clip-rule="evenodd"
+						></path>
+					</svg>
+					<span class="sr-only">Close modal</span>
+				</button>
+				<div class="py-6 px-6 lg:px-8">
+					<h3 class="mb-4 text-xl font-medium text-gray-900  text-white">
+						Create new team
+					</h3>
+					<form class="mt-8" @submit.prevent="createNewTeam()">
+						<div>
+							<label
+								for="email"
+								class="block mb-2 text-sm font-medium text-gray-200  text-gray-300"
+								>Team name</label
+							>
+							<input
+								type="text"
+								class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  bg-gray-600  border-gray-500  placeholder-gray-400  text-white"
+								placeholder="my new team"
+								v-model="newTeamName"
+							/>
+						</div>
 
-                        <button
-                        type="submit"
-                        class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  bg-blue-600  hover:bg-blue-700  focus:ring-blue-800"
-                        >
-                        Create team
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>	  
+						<button	type="submit" class="w-full mt-0"> Create team </button>
+					</form>
+				</div>
+			</div>
+		</div>
+  	</div>	  
 </template>
 
 <script>
