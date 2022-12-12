7c6fbd1bba2949eb96a20ce61bc0ff0e488db768
Tim Joyce <tim@evendrop.com>
2022-12-12 10:55:35 -0500

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel HubSpot Starter Kit</title>
 
-	@vite('resources/css/app.css')
+	@php
+    	$manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+    @endphp
+	<script type="module" src="/build/{{ $manifest['resources/js/app.js']['file'] }}"></script>
+	<link rel="stylesheet" href="/build/{{ $manifest['resources/js/app.js']['css'][0] }}">
 	@routes
 </head>
 <body class="sidebar-expanded">
 	<div id="app" class="flex h-screen overflow-hidden"></div>
 
-	@vite('resources/js/app.js')
+	<script type="module" src="http://localhost:3000/@@vite/client"></script>
+    <script type="module" src="http://localhost:3000/resources/js/app.js"></script>
 </body> 
 </html>
