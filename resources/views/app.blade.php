69d3eb4aedfd7297f9f70ac08465e5f4159168e2
Tim Joyce <tim@evendrop.com>
2022-12-12 10:51:05 -0500

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel HubSpot Starter Kit</title>
 
-	@vite('resources/css/app.css')
 	@routes
 </head>
 <body class="sidebar-expanded">
 	<div id="app" class="flex h-screen overflow-hidden"></div>
 
-	@vite('resources/js/app.js')
 </body> 
 </html>
